d7d1b2215214f7f3c81abadb63e954bc3638bee4
lakshy pachory <luckyron1279@gmail.com>
2020-02-19 10:36:26 +0530

    add view engine

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 router.get('/',homeController.home);
 router.use('/users',require('./user'));
-router.use('/posts',require('./post'));
+//router.use('/posts',require('./post'));
 //for any other excess from here
 //router.use(routername,require('router file))
 module.exports=router;
