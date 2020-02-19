aea06709391ac2c3bb78fd790bacdbbdb576e28e
lakshy pachory <luckyron1279@gmail.com>
2020-02-19 10:24:47 +0530

    add new route and controllers name post and user

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,8 @@ const router =express.Router();
 const homeController=require('../controllers/home_controller');
 console.log('router loaded');
 router.get('/',homeController.home);
+router.use('/users',require('./user'));
+router.use('/posts',require('./post'));
+//for any other excess from here
+//router.use(routername,require('router file))
 module.exports=router;
